fa499f09b871f8d47045b1c20f57a13a31bd520c
ace <hotyss@hotmail.com>
2022-07-11 17:46:56 +0800

    重构文本输入框资源名

---
+++ src/main/kotlin/com/githubyss/mobile/common/res/text_field/TextFieldColorsOutlined.kt
@@ -0,0 +1,95 @@
+package com.githubyss.mobile.common.res.text_field
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+
+/** Text field outlined black colors. */
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedBackgroundNormal: Color
+    get() = Color(0xFF151515)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedTextNormal: Color
+    get() = Color(0xFF353D44)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedTextDisabled: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLabelFocused: Color
+    get() = Color(0x993399FF)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLabelUnfocused: Color
+    get() = Color(0xFF3399FF)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLabelDisabled: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLabelError: Color
+    get() = Color(0xFFFFFF00)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedPlaceholderNormal: Color
+    get() = Color(0x66228B22)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedPlaceholderDisabled: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedCursorNormal: Color
+    get() = Color(0xFF353D44)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedCursorError: Color
+    get() = Color(0xFFFFFF00)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedIndicatorFocused: Color
+    get() = Color(0xFF228B22)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedIndicatorUnfocused: Color
+    get() = Color(0xFF353D44)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedIndicatorDisabled: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedIndicatorError: Color
+    get() = Color(0xFFFF5500)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLeadingIconNormal: Color
+    get() = Color(0xFF228B22)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLeadingIconDisabled: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedLeadingIconError: Color
+    get() = Color(0xFFFFFF00)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedTrailingIconNormal: Color
+    get() = Color(0xFF228B22)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedTrailingIconDisabled: Color
+    get() = Color(0xFFD3D3D3)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedTrailingIconError: Color
+    get() = Color(0xFFFFFF00)
+
+@Stable
+inline val Color.Companion.textFieldBlackOutlinedBorderColor: Color
+    get() = Color(0x00000000)
